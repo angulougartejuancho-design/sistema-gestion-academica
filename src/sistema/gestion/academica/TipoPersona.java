3831b96b41f8754bab6df25f3fcab3b14f21b4c3
angulougartejuancho <angulougartejuancho@gmail.com>
2026-05-31 15:51:41 -0600

    Implementacion de la estructura base del sistema con Persona, Direccion, TipoPersona y Utilidades

---
+++ src/sistema/gestion/academica/TipoPersona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package sistema.gestion.academica;
+
+/**
+ *
+ * @author angul
+ */
+public enum TipoPersona {
+    DOCENTE,
+    ESTUDIANTE
+    
+}
---
+++ src/sistema/gestion/academica/Utilidades.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.gestion.academica;
+
+/**
+ *
+ * @author angul
+ */
+public class Utilidades {
+
+    // Metodo estatico de bienvenida
+    public static void mostrarBienvenida() {
+
+        System.out.println("========================================");
+        System.out.println("     SISTEMA DE GESTIÓN ACADÉMICA");
+        System.out.println("========================================");
+        System.out.println("      Bienvenido al sistema");
+        System.out.println("========================================");
+    }
+}
